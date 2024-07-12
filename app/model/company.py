a30075003a997a65c518bda6f16042c422c25a37
GaryChiangCC <garychiang@hotaimotor.com.tw>
2024-07-12 09:47:27 +0800

    model modified

--- app/model/company.py
+++ app/model/company.py
@@ -13,7 +13,7 @@ class Information(Base):
     __tablename__ = "information"
     __table_args__ = {"comment": "公司資訊檔"}
 
-    id = Column(BigInteger, primary_key=True)
+    id = Column(BigInteger, primary_key=True, autoincrement=True)
     unified_no = Column(String(16), nullable=True, comment="統一編號")
     c_name = Column(String(128), nullable=True, comment="公司中文名稱")
     e_name = Column(String(128), nullable=True, comment="公司英文名稱")
@@ -22,7 +22,7 @@ class Information(Base):
     e_address = Column(String(512), nullable=True, comment="公司所在地(英文)")
     approve_date = Column(String(32), nullable=True, comment="設立核准日期")
     modified_date = Column(String(32), nullable=True, comment="最後核准變更日期")
-    cancel = Column(Integer, nullable=False, comment="是否生效")
+    cancel = Column(Integer, nullable=False, comment="是否生效", index=True)
     create_time = Column(DateTime, nullable=False, comment="建立時間")
     update_time = Column(DateTime, nullable=True, comment="修改時間")
 
